77ae927f4eb15817c86f049a206b2c56f71ffcf9
Dago R <dagoramirezserna@gmail.com>
2020-10-07 16:42:35 -0500

    Se modifica el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,14 +8,14 @@ const ICON_INSTAGRAM = <FontAwesomeIcon icon={faInstagramSquare} />;
 
 export default function Footer() {
     return (
-        <div className="bg-green-jhonny text-center flex flex-col md:flex-row items-start md:items-center justify-around py-2" >
+        <div className="bg-green-jhonny text-center flex flex-col sm:flex-row items-start md:items-center justify-around py-2" >
             <div className="flex flex-col items-start ml-5 md:ml-0" >
                 <h1 className="text-lg text-white font-bold underline" >Corporativo</h1>
                 <a href="/" className="text-sm text-white hover:text-gray-400" >Misión</a>
                 <a href="/" className="text-sm text-white hover:text-gray-400" >Visión</a>
                 <a href="/" className="text-sm text-white hover:text-gray-400" >Nuestro Municipio</a>
             </div>
-            <div className="flex flex-col items-start my-3 md:my-0  ml-5 md:ml-0" >
+            <div className="flex flex-col items-start my-3 sm:my-0  ml-5 md:ml-0" >
                 <h1 className="text-lg text-white font-bold underline" >Donde Jhonny</h1>
                 <h1 className="text-sm text-white" >Cel: 315 202 9674</h1>
                 <h1 className="text-sm text-white" >Correo: donde-jhonny@correo.com</h1>
